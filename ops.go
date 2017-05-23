f9d4af7c2bd76248c539e1b83decaf0f01554f79
Paolo Galeone <nessuno@nerdz.eu>
2017-05-23 15:28:31 +0200

    {ops,types}.go: added. This 2 files add general operations and type supports

---
+++ ops.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2017 Paolo Galeone. All right reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tfgo
+
+import (
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+)
+
+// Batchify creates a batch of tensors, concatenating them along the first dimension
+func Batchify(scope *op.Scope, tensors []tf.Output) tf.Output {
+	s := scope.SubScope("batchify")
+	var tensors4d []tf.Output
+	for _, tensor := range tensors {
+		tensors4d = append(tensors4d, op.ExpandDims(s.SubScope("ExpandDims"), tensor, op.Const(s.SubScope("axis"), []int32{0})))
+	}
+	return op.ConcatV2(s.SubScope("ConcatV2"), tensors4d, op.Const(s.SubScope("axis"), int32(0)))
+}
+
+// Cast casts value to the specified dtype
+func Cast(scope *op.Scope, value tf.Output, dtype tf.DataType) tf.Output {
+	if value.DataType() == dtype {
+		return value
+	}
+	return op.Cast(scope.SubScope("Cast"), value, dtype)
+}
+
+// NewScope creates a new *op.Scope
+func NewScope() *op.Scope {
+	return op.NewScope()
+}
+
+// Const creates a constant value within the specified scope
+func Const(scope *op.Scope, value interface{}) tf.Output {
+	return op.Const(scope.SubScope("Const"), value)
+}
+
+// Exec creates the computation graph from the scope, then executes
+// the operations required to compute each element of tensors.
+// Node in the graph can be overwritten with feedDict.
+// The session options can be specified using the session parameter.
+// Returns the evaluated tensors. Panics on error.
+func Exec(scope *op.Scope, tensors []tf.Output, feedDict map[tf.Output]*tf.Tensor, options *tf.SessionOptions) []*tf.Tensor {
+	graph, err := scope.Finalize()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if sess, err := tf.NewSession(graph, options); err == nil {
+		if results, err := sess.Run(feedDict, tensors, nil); err == nil {
+			return results
+		} else {
+			panic(err)
+		}
+	} else {
+		panic(err)
+	}
+}
---
+++ types.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2017 Paolo Galeone. All right reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tfgo
+
+import (
+	"fmt"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"math"
+)
+
+// IsInteger returns true if dtype is a tensorflow integer type
+func IsInteger(dtype tf.DataType) bool {
+	switch dtype {
+	case tf.Int8, tf.Int16, tf.Int32, tf.Int64, tf.Uint8, tf.Uint16, tf.Qint8, tf.Qint16, tf.Qint32, tf.Quint8, tf.Quint16:
+		return true
+	default:
+		return false
+	}
+}
+
+// IsFloat returns true if dtype is a tensorfow float type
+func IsFloat(dtype tf.DataType) bool {
+	switch dtype {
+	case tf.Double, tf.Float, tf.Half:
+		return true
+	default:
+		return false
+	}
+}
+
+// MaxValue returns the maximum value accepted for the dtype
+func MaxValue(dtype tf.DataType) float64 {
+	switch dtype {
+	case tf.Double:
+		return math.MaxFloat64
+	case tf.Float:
+		return math.MaxFloat32
+	case tf.Half:
+		return math.MaxFloat32 / math.Pow(2, 16)
+	case tf.Int16:
+		return math.MaxInt16
+	case tf.Int32:
+		return math.MaxInt32
+	case tf.Int64:
+		return math.MaxInt64
+	case tf.Int8:
+		return math.MaxInt8
+	case tf.Uint16:
+		return math.MaxUint16
+	case tf.Uint8:
+		return math.MaxUint8
+		// No support for Quantized types
+	}
+	panic(fmt.Sprintf("dtype %d not supported", dtype))
+}
+
+// MinValue returns the minimin representable value for the specified dtype
+func MinValue(dtype tf.DataType) float64 {
+	switch dtype {
+	case tf.Double:
+		return math.SmallestNonzeroFloat64
+	case tf.Float:
+		return math.SmallestNonzeroFloat32
+	case tf.Half:
+		// According to: https://www.khronos.org/opengl/wiki/Small_Float_Formats
+		return 6.10 * math.Pow10(-5)
+	case tf.Int16:
+		return math.MinInt16
+	case tf.Int32:
+		return math.MinInt32
+	case tf.Int64:
+		return math.MinInt64
+	case tf.Int8:
+		return math.MinInt8
+	case tf.Uint16, tf.Uint8:
+		return 0
+		// No support for Quantized types
+	}
+	panic(fmt.Sprintf("dtype %d not supported", dtype))
+}
